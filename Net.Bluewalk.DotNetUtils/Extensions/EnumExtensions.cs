b55d2c7b618251d090a20b31eaa8fdb68dd096f7
Délano <bluewalk@users.noreply.github.com>
2020-07-28 10:33:20 +0200

    Update EnumExtensions.cs

--- Net.Bluewalk.DotNetUtils/Extensions/EnumExtensions.cs
+++ Net.Bluewalk.DotNetUtils/Extensions/EnumExtensions.cs
@@ -15,5 +15,75 @@ namespace Net.Bluewalk.DotNetUtils.Extensions
         {
             return parameters.Contains(value);
         }
+        
+        /// <summary>
+        /// Determines if enum flags overlap another enum flags
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static bool HasOverlappingFlags<T>(this T a, T b) where T : Enum
+        {
+            var values = Enum.GetValues(typeof(T)).Cast<T>().ToList();
+
+            return values.Any(v => a.HasFlag(v) && b.HasFlag(v));
+        }
+        
+        /// <summary>
+        /// Get enum items
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static List<EnumItem> GetEnumItems(Type t)
+        {
+            var values = Enum.GetValues(t);
+            var result = new List<EnumItem>();
+
+            foreach (var item in values)
+            {
+                var name = Enum.GetName(t, item);
+
+                result.Add(new EnumItem()
+                {
+                    Name = name,
+                    NamePretty = Regex.Replace(name, "(?<=.)([A-Z][a-z])", " $0"),
+                    Value = item
+                });
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get enum items
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static List<EnumItem> GetEnumItems<T>()
+        {
+            return GetEnumItems(typeof(T));
+        }
+
+        /// <summary>
+        /// Enum item
+        /// </summary>
+        public class EnumItem
+        {
+            /// <summary>
+            /// Name
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            /// Pretty name (formatted)
+            /// </summary>
+            public string NamePretty { get; set; }
+
+            /// <summary>
+            /// Value
+            /// </summary>
+            public object Value { get; set; }
+        }
     }
 }
