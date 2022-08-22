bb967e8d4ae62dfde144728f2fd2e403421b4626
bluewalk <noreply@bluewalk.net>
2022-08-22 19:08:25 +0200

    Added BytesToString and NET5 and NET6 native binaries

--- Net.Bluewalk.DotNetUtils/Extensions/LongExtensions.cs
+++ Net.Bluewalk.DotNetUtils/Extensions/LongExtensions.cs
@@ -14,5 +14,21 @@ namespace Net.Bluewalk.DotNetUtils.Extensions
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return epoch.AddSeconds(value);
         }
+
+        /// <summary>
+        /// Converts bytes into a readable string
+        /// </summary>
+        /// <param name="byteCount"></param>
+        /// <returns></returns>
+        public static string BytesToString(this long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            var bytes = Math.Abs(byteCount);
+            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num) + suf[place];
+        }
     }
 }
